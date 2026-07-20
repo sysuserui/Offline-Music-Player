9a6bdee6b95e144d4a08f0efad28a5b9d1e5fc70
sysuserui <sysuserui@gmail.com>
2026-07-20 21:26:44 +0530

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.7.1")
+
+    implementation("com.google.android.material:material:1.14.0")
+
+    implementation("androidx.media3:media3-common:1.10.1")
+
+    implementation("androidx.media3:media3-exoplayer:1.10.1")
+
+    implementation("androidx.media3:media3-ui:1.10.1")
+
+    implementation("androidx.media3:media3-session:1.10.1")
+
+}
